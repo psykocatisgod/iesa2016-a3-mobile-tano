276f5ede35a3779b00fc6be1d1e6a7219a03da40
Paul Karampournis <paul.karampournis@spyrit.net>
2016-04-14 17:06:11 +0200

    v1

--- tano/tano/platforms/ios/platform_www/cordova_plugins.js
+++ tano/tano/platforms/ios/platform_www/cordova_plugins.js
@@ -127,6 +127,38 @@ module.exports = [
         "clobbers": [
             "window.sms"
         ]
+    },
+    {
+        "file": "plugins/cordova-plugin-geolocation/www/Coordinates.js",
+        "id": "cordova-plugin-geolocation.Coordinates",
+        "pluginId": "cordova-plugin-geolocation",
+        "clobbers": [
+            "Coordinates"
+        ]
+    },
+    {
+        "file": "plugins/cordova-plugin-geolocation/www/PositionError.js",
+        "id": "cordova-plugin-geolocation.PositionError",
+        "pluginId": "cordova-plugin-geolocation",
+        "clobbers": [
+            "PositionError"
+        ]
+    },
+    {
+        "file": "plugins/cordova-plugin-geolocation/www/Position.js",
+        "id": "cordova-plugin-geolocation.Position",
+        "pluginId": "cordova-plugin-geolocation",
+        "clobbers": [
+            "Position"
+        ]
+    },
+    {
+        "file": "plugins/cordova-plugin-geolocation/www/geolocation.js",
+        "id": "cordova-plugin-geolocation.geolocation",
+        "pluginId": "cordova-plugin-geolocation",
+        "clobbers": [
+            "navigator.geolocation"
+        ]
     }
 ];
 module.exports.metadata = 
@@ -135,7 +167,8 @@ module.exports.metadata =
     "cordova-plugin-whitelist": "1.2.1",
     "cordova-plugin-camera": "2.1.1",
     "cordova-plugin-contacts": "2.0.1",
-    "com.cordova.plugins.sms": "0.1.10"
+    "com.cordova.plugins.sms": "0.1.10",
+    "cordova-plugin-geolocation": "2.1.0"
 }
 // BOTTOM OF METADATA
 });
---
+++ tano/tano/www/js/map.js
@@ -0,0 +1,148 @@
+$(function () {
+
+
+
+    'use strict';
+
+   var carte, infowindow, final = [],
+      markers = [],
+      LatiLong;
+
+   function googleMaps() {
+
+    var onSuccess = function(position){
+        LatiLong = {
+         lat: position.coords.latitude,
+         lng: position.coords.longitude
+        };
+
+      var mycoord = new google.maps.LatLng( LatiLong.lat, LatiLong.lng );
+
+      var options = {
+         center: mycoord,
+         zoom: 13,
+         mapTypeId: google.maps.MapTypeId.ROADMAP,
+         draggable: true,
+         scrollwheel: false
+      };
+
+      var carte = new google.maps.Map(document.getElementById("map"), options),
+        infowindow = new google.maps.InfoWindow();
+
+       var marker = new google.maps.Marker({
+              position: mycoord,
+              map: carte
+            });
+
+      var service = new google.maps.places.PlacesService(carte);
+
+      var input = document.getElementById('pac-input');
+      var searchBox = new google.maps.places.SearchBox(input);
+      carte.controls[google.maps.ControlPosition.TOP_LEFT].push(input);
+
+      carte.addListener('bounds_changed', function () {
+         searchBox.setBounds(carte.getBounds());
+      });
+
+      searchBox.addListener('places_changed', function () {
+         var places = searchBox.getPlaces();
+
+         if (places.length == 0) {
+            return;
+         }
+         marker.setMap(null);
+         // Clear out the old markers.
+         markers.forEach(function (marker) {
+            marker.setMap(null);
+         });
+         markers = [];
+
+         // For each place, get the icon, name and location.
+         var bounds = new google.maps.LatLngBounds();
+         places.forEach(function (place) {
+            var icon = {
+               url: place.icon,
+               size: new google.maps.Size(71, 71),
+               origin: new google.maps.Point(0, 0),
+               anchor: new google.maps.Point(17, 34),
+               scaledSize: new google.maps.Size(25, 25)
+            };
+
+            // Create a marker for each place.
+            markers.push(new google.maps.Marker({
+               map: carte,
+               icon: icon,
+               title: place.name,
+               position: place.geometry.location
+            }));
+
+            if (place.geometry.viewport) {
+               // Only geocodes have viewport.
+               bounds.union(place.geometry.viewport);
+            } else {
+               bounds.extend(place.geometry.location);
+            }
+            LatiLong = {
+               lat: place.geometry.location.lat(),
+               lng: place.geometry.location.lng()
+            };
+         });
+         carte.fitBounds(bounds);
+
+         service.nearbySearch({
+            location: LatiLong,
+            radius: 300,
+            types: ['restaurant']
+         }, callback);
+      });
+
+    }//onSuccess
+     navigator.geolocation.getCurrentPosition(onSuccess, onError);
+
+   }
+
+   function callback(results, status) {
+      if (status === google.maps.places.PlacesServiceStatus.OK) {
+    console.log('ici');
+         for (var i = 0; i < results.length; i++) {
+            createMarker(results[i]);
+            var reponse = document.querySelector('.list-group'),
+               list = document.createElement('li'),
+               valeur = results[i]['name'];
+            final.push(results[i]['name']);
+
+            reponse.appendChild(list);
+            list.appendChild(document.createTextNode(final[i]));
+         }
+      }
+   }
+
+   function createMarker(place) {
+      var placeLoc = place.geometry.location;
+      var marker = new google.maps.Marker({
+         map: carte,
+         position: place.geometry.location
+      });
+
+      google.maps.event.addListener(marker, 'click', function () {
+         infowindow.setContent(place.name);
+         infowindow.open(carte, this);
+      });
+   }
+
+
+
+
+   function onError(error) {
+        alert('code: '    + error.code    + '\n' +
+              'message: ' + error.message + '\n');
+    }
+
+
+   // googleMaps();
+   google.maps.event.addDomListener(window, 'load', googleMaps);
+
+
+
+
+});
