93581ab6a0d3cd8680a451471d97c3508c2f7ea6
Paul Karampournis <paul.karampournis@spyrit.net>
2016-04-15 16:02:35 +0200

    Add design

--- tano/tano/platforms/android/assets/www/js/langue.js
+++ tano/tano/platforms/android/assets/www/js/langue.js
@@ -7,7 +7,6 @@ function onDeviceReady() {
     var onSuccess = function(language)
     {
         alert('language: ' + language.value + '\n');
-        document.getElementById('curr-locale').innerHTML(language.value);
         // document.querySelectorAll('.curr-locale').innerHTML = language.value;
     }
     function onError()
