40f9b482fefb3e2cf895a55a7fa20b36776c0c7d
Yuz-Agency <agence.yuz@gmail.com>
2016-04-15 16:25:25 +0200

    Langue (2)

--- tano/tano/www/js/langue.js
+++ tano/tano/www/js/langue.js
@@ -15,8 +15,44 @@ $(function () {
 	   {
 	       alert('Error getting language\n');
 	   }
-	   navigator.globalization.getPreferredLanguage(onSuccess, onError);
+	   navigator.globalization.getLocaleName(onSuccess, onError);
 
 	}
+
+	$('.curr-locale').on('click', function(){
+		$('.dropdown-menu').toggleClass('active');
+	});
+
+	// function ajaxify(){
+		 // $.ajax({
+   //              // url : "http://www.titouanpierre.com/json/_infos.json",
+   //              url : "http://localhost/iesa/mobile/tano/iesa2016-a3-mobile-tano/tano/json/_infos.json",
+   //              type: 'POST',
+   //              dataType: 'json',
+   //              success : function(data){
+
+   //                  console.log(data);
+   //                  for (var i = 0; i < data.length; i++) {
+   //                  	$('.datas').append('<li>'+data[i].id+'</li>');
+   //              	}//endfor
+   //              },//Fin Success
+		 //        error: function(){
+		 //            alert('Erreur Serveur');
+		 //        }//Fin Error//endSuccess
+   //      });
+
+ //   		$.getJSON("http://titouanpierre.com/json/_infos.json",function(result){
+	// 	  	console.log(result);
+	// 	  	$.each(result, function(i, field){
+	// 	  		console.log(i, field);
+
+	// 	     });
+	//     });
+
+	// }	
+
+	// ajaxify();
+
+
 
 });
