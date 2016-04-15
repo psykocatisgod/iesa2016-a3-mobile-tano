0f2cb9cee8d76b628038fad19d7ef0927215bc3b
Paul Karampournis <paul.karampournis@spyrit.net>
2016-04-15 16:33:15 +0200

    correction langue

--- tano/tano/www/js/langue.js
+++ tano/tano/www/js/langue.js
@@ -1,18 +1,58 @@
-document.addEventListener("deviceready", onDeviceReady, false);
+$(function () {
 
-// Cordova is ready
-//
-function onDeviceReady() {
+    document.addEventListener("deviceready", onDeviceReady, false);
 
-    var onSuccess = function(language)
-    {
-        alert('language: ' + language.value + '\n');
-        // document.querySelectorAll('.curr-locale').innerHTML = language.value;
+    // Cordova is ready
+    //
+    function onDeviceReady() {
+
+        var onSuccess = function(language)
+        {
+            // alert('language: ' + language.value + '\n');
+            $('.curr-locale, .locale').text(language.value);
+        }
+        function onError()
+        {
+            alert('Error getting language\n');
+        }
+        navigator.globalization.getLocaleName(onSuccess, onError);
+
     }
-    function onError()
-    {
-        alert('Error getting language\n');
-    }
-    navigator.globalization.getPreferredLanguage(onSuccess, onError);
 
-}
+    $('.curr-locale').on('click', function(){
+        $('.dropdown-menu').toggleClass('active');
+    });
+
+    // function ajaxify(){
+    // $.ajax({
+    //              // url : "http://www.titouanpierre.com/json/_infos.json",
+    //              url : "http://localhost/iesa/mobile/tano/iesa2016-a3-mobile-tano/tano/json/_infos.json",
+    //              type: 'POST',
+    //              dataType: 'json',
+    //              success : function(data){
+
+    //                  console.log(data);
+    //                  for (var i = 0; i < data.length; i++) {
+    //                  	$('.datas').append('<li>'+data[i].id+'</li>');
+    //              	}//endfor
+    //              },//Fin Success
+    //        error: function(){
+    //            alert('Erreur Serveur');
+    //        }//Fin Error//endSuccess
+    //      });
+
+    //   		$.getJSON("http://titouanpierre.com/json/_infos.json",function(result){
+    // 	  	console.log(result);
+    // 	  	$.each(result, function(i, field){
+    // 	  		console.log(i, field);
+
+    // 	     });
+    //     });
+
+    // }
+
+    // ajaxify();
+
+
+
+});
