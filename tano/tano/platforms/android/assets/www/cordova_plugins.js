8786862d15de56bb4acae81e7140ca0ead02e691
Yuz-Agency <agence.yuz@gmail.com>
2016-04-15 14:29:05 +0200

    Langue Ini(2)

--- tano/tano/platforms/android/assets/www/cordova_plugins.js
+++ tano/tano/platforms/android/assets/www/cordova_plugins.js
@@ -1,7 +1,24 @@
 cordova.define('cordova/plugin_list', function(require, exports, module) {
-module.exports = [];
+module.exports = [
+    {
+        "file": "plugins/cordova-plugin-globalization/www/GlobalizationError.js",
+        "id": "cordova-plugin-globalization.GlobalizationError",
+        "clobbers": [
+            "window.GlobalizationError"
+        ]
+    },
+    {
+        "file": "plugins/cordova-plugin-globalization/www/globalization.js",
+        "id": "cordova-plugin-globalization.globalization",
+        "clobbers": [
+            "navigator.globalization"
+        ]
+    }
+];
 module.exports.metadata = 
 // TOP OF METADATA
-{}
+{
+    "cordova-plugin-globalization": "1.0.4-dev"
+}
 // BOTTOM OF METADATA
 });
--- tano/tano/platforms/ios/www/cordova_plugins.js
+++ tano/tano/platforms/ios/www/cordova_plugins.js
@@ -1,9 +1,25 @@
 cordova.define('cordova/plugin_list', function(require, exports, module) {
-module.exports = [];
+module.exports = [
+    {
+        "file": "plugins/cordova-plugin-globalization/www/GlobalizationError.js",
+        "id": "cordova-plugin-globalization.GlobalizationError",
+        "clobbers": [
+            "window.GlobalizationError"
+        ]
+    },
+    {
+        "file": "plugins/cordova-plugin-globalization/www/globalization.js",
+        "id": "cordova-plugin-globalization.globalization",
+        "clobbers": [
+            "navigator.globalization"
+        ]
+    }
+];
 module.exports.metadata = 
 // TOP OF METADATA
 {
-    "cordova-plugin-whitelist": "1.2.1"
+    "cordova-plugin-whitelist": "1.2.1",
+    "cordova-plugin-globalization": "1.0.4-dev"
 }
 // BOTTOM OF METADATA
 });
