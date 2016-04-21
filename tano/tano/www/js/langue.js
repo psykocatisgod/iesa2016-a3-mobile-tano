0157039f5b52da273d6d99588159c88d95cdacf2
Paul Karampournis <paul.karampournis@spyrit.net>
2016-04-21 12:20:24 +0200

    Adding remote for ios & android

--- tano/tano/www/js/langue.js
+++ tano/tano/www/js/langue.js
@@ -1,57 +1,64 @@
 $(function () {
+    
+	document.addEventListener("deviceready", onDeviceReady, false);
 
-    document.addEventListener("deviceready", onDeviceReady, false);
+  // Cordova is ready
+  //
+  	function onDeviceReady() {
+	     
+	   var onSuccess = function(language)
+	   {
+	   		// alert('language: ' + language.value + '\n');
+	   		// ajaxify(language.value);
+	   		ajaxify(language.value);
+	   		$('.curr-locale, .locale').text(language.value);	
+   		}
+	   function onError()
+	   {
+	       alert('Error getting language\n');
+	   }
+	   navigator.globalization.getLocaleName(onSuccess, onError);
 
-    // Cordova is ready
-    //
-    function onDeviceReady() {
+	}
 
-        var onSuccess = function(language)
-        {
-            // alert('language: ' + language.value + '\n');
-            $('.curr-locale, .locale').text(language.value);
-        }
-        function onError()
-        {
-            alert('Error getting language\n');
-        }
-        navigator.globalization.getLocaleName(onSuccess, onError);
+	$('.curr-locale').on('click', function(){
+		$('.dropdown-menu').toggleClass('active');
+	});
 
-    }
+	$('.ch-lang').on('click', function(){
+		ajaxify($(this).data('loc'));
+	});
 
-    $('.curr-locale').on('click', function(){
-        $('.dropdown-menu').toggleClass('active');
-    });
+	function ajaxify(lang){
+		 $.ajax({
+            url : "http://www.titouanpierre.com/json/read.php",
+            type: 'GET',
+            dataType: 'json',
+            success : function(data){
+            	data = data[0][lang];
+  				console.log(data);   
+  				$('.datas').html('');
+  				for (var i = 0; i < data.length; i++) {
+					var html = '<div class="resto-'+data[i].id+'">'+
+									'<h2>'+data[i].name+'</h2>'+
+									'<div class="description">'+data[i].description+'</div>'
+								'</div>';
+					$('.datas').append(html);
+				}           
+            },//Fin Success
+	        error: function(xhr, message, errorThrown ){
+	            console.log(xhr);
+	            console.log(message);
+	            console.log(errorThrown);
 
-    // function ajaxify(){
-    // $.ajax({
-    //              // url : "http://www.titouanpierre.com/json/_infos.json",
-    //              url : "http://localhost/iesa/mobile/tano/iesa2016-a3-mobile-tano/tano/json/_infos.json",
-    //              type: 'POST',
-    //              dataType: 'json',
-    //              success : function(data){
+	        }//Fin Error//endSuccess
+        });
 
-    //                  console.log(data);
-    //                  for (var i = 0; i < data.length; i++) {
-    //                  	$('.datas').append('<li>'+data[i].id+'</li>');
-    //              	}//endfor
-    //              },//Fin Success
-    //        error: function(){
-    //            alert('Erreur Serveur');
-    //        }//Fin Error//endSuccess
-    //      });
+	}	
 
-    //   		$.getJSON("http://titouanpierre.com/json/_infos.json",function(result){
-    // 	  	console.log(result);
-    // 	  	$.each(result, function(i, field){
-    // 	  		console.log(i, field);
+	// ajaxify('fr_FR');
 
-    // 	     });
-    //     });
-
-    // }
-
-    // ajaxify();
+	
 
 
 
