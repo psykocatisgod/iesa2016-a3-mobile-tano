07e2f9c8933e3906e286443b620bba397de71c31
Paul Karampournis <paul.karampournis@spyrit.net>
2016-04-21 10:43:29 +0200

    Adding Network Information into app

--- tano/tano/www/js/index.js
+++ tano/tano/www/js/index.js
@@ -36,9 +36,24 @@ var app = {
         app.receivedEvent('deviceready');
         app.setupFindContactsButtonCallback();
         app.setupPhotoFindButton();
+        app.checkConnection();
 
     },
 
+    checkConnection: function() {
+        var networkState = navigator.connection.type;
+        var states = {};
+        states[Connection.UNKNOWN]  = 'Unknown connection';
+        states[Connection.ETHERNET] = 'Ethernet connection';
+        states[Connection.WIFI]     = 'WiFi connection';
+        states[Connection.CELL_2G]  = 'Cell 2G connection';
+        states[Connection.CELL_3G]  = 'Cell 3G connection';
+        states[Connection.CELL_4G]  = 'Cell 4G connection';
+        states[Connection.CELL]     = 'Cell generic connection';
+        states[Connection.NONE]     = 'No network connection';
+
+        document.getElementById('network').innerHTML = 'Votre connexion: ' + states[networkState];
+    },
 
     setupFindContactsButtonCallback: function () {
         $('#findButton').click(function () {
